598b5a0ddb9e90786893b93e99aaaa1192adc8b7
Brody McCoy <79420042+mccoybrody@users.noreply.github.com>
2021-04-02 18:51:06 -0500

    favExercises

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,8 +1,20 @@
 
 hmbMenu.onclick=function(s){
-  if (typeof(s) == "object") 
+  if (typeof(s) == "object") {
     return
-  /* add a switch here to take the user to the form
-     they chose. 
-  */
-}
+    }else{
+  switch (s) {
+case "Dessert Voting":
+        ChangeForm(dessertVoting);
+        break;
+case "Describe You":
+        ChangeForm(describeYou);
+        break;
+case "Favorite Exercise":
+        ChangeForm(favExercises);
+        break;
+case "Mobile Nav":
+        ChangeForm(mobileNav);
+        break;
+        }
+}}
